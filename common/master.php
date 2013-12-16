e8a1760a95c53db662954500e24640f0a6f534ef
Hamish Hutchings <hamhut1066@gmail.com>
2013-12-16 23:44:31 +0000

    add bootstrap

---
+++ common/master.php
@@ -0,0 +1,11 @@
+<!--head-->
+<head>
+
+<link rel="stylesheet" href="/dist/css/bootstrap.min.css">
+<script scr="/dist/js/bootstrap.min.js"></script>
+<title id="title">hamhut1066</title>
+</head>
+<body>
+
+<p>hello world</p>
+</body>
