d0c5f611b59327a06e2a7d007b35486c06e4f96d
Hamish Hutchings <hamhut1066@gmail.com>
2014-01-23 21:11:08 +0000

    changes to master page

--- common/master.php
+++ common/master.php
@@ -12,16 +12,18 @@ require('helpers.php');
     <?php require('menu.php'); ?>
 
 <div id="main" class="container clear-top">
-      <div class="starter-template">
+      <div class="hidden-print starter-template">
         <h1>Ajax Site</h1>
         <p class="lead">hamish's ajax site</p>
       </div>
 <div class="row">
 <div class="col-xs-2">
+<div class="hidden-print">
 <!-- I can now add random things in here If I feel so inclined -->
 <p>chat client could be added here</p>
 </div>
-<div class="col-xs-8 hidden-phone">
+</div>
+<div class="col-xs-8">
 <span id="content">
 <!--the content of the page -->
 <?php
